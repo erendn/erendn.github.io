5d0c77580a76889796e88cc966b9c9518c4563d5
Eren <biarmic@gmail.com>
2021-02-09 16:33:11 +0300

    new design

--- scripts/homepage.js
+++
@@ -1,8 +0,0 @@
-function update() {
-    textFit(document.getElementById('name-container'));
-    textFit(document.getElementById('occupation-container'));
-    var contentTitles = document.getElementsByClassName('content-title');
-    for (var i = 0; i < contentTitles.length; i++) {
-        textFit(contentTitles[i]);
-    }
-}
