22e56727ef45e983eda270a9cc9a68a1c1ceee76
erendo <eren.dogan@ozu.edu.tr>
2022-01-27 23:06:43 +0300

    Fix node name

--- utils.js
+++ utils.js
@@ -2,22 +2,23 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 async function last_update() {
     // Get the element of the document
-    var doc_element = document.getElementById("update");
+    const doc_element = document.getElementById("update");
+    const lang = doc_element.getAttribute("name");
     // Get the last commit on this repo
     const octokit = new Octokit();
     var file = "index.html";
-    if (doc_element.nodeName != "en") {
-        file = doc_element.nodeName + "/" + file;
+    if (lang != "en") {
+        file = lang + "/" + file;
     }
     var commit = await octokit.request("GET /repos/erendo/erendo.github.io/commits/main?path=" + file);
     // Get the date of the last commit
     var date = new Date(commit.data.commit.committer.date);
     // Convert the date string to correct format
     var str = ""
-    if (doc_element.nodeName == "en") { // American English
+    if (lang == "en") { // American English
         var date_str = date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
         str = "Last updated on " + date_str;
-    } else if (doc_element.nodeName == "tr") { // Turkish
+    } else if (lang == "tr") { // Turkish
         var date_str = date.toLocaleDateString("tr-TR", { year: 'numeric', month: 'long', day: 'numeric' });
         str = "En son " + date_str + " tarihinde güncellendi";
     }
