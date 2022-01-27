a9ca5652848db3d87a25c3cdda50a63c471b3b5c
erendo <eren.dogan@ozu.edu.tr>
2022-01-27 23:37:42 +0300

    Revert last changes

--- utils.js
+++ utils.js
@@ -1,29 +1,16 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 async function last_update() {
-    // Get the element of the document
-    const doc_element = document.getElementById("update");
-    const lang = doc_element.getAttribute("name");
     // Get the last commit on this repo
     const octokit = new Octokit();
-    var file = "index.html";
-    if (lang != "en") {
-        file = lang + "/" + file;
-    }
-    var commit = await octokit.request("GET /repos/erendo/erendo.github.io/commits/main?path=" + file);
+    var commit = await octokit.request("GET /repos/erendo/erendo.github.io/commits/main");
     // Get the date of the last commit
-    var date = new Date(commit.data.commit.committer.date);
-    // Convert the date string to correct format
-    var str = ""
-    if (lang == "en") { // American English
-        var date_str = date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
-        str = "Last updated on " + date_str;
-    } else if (lang == "tr") { // Turkish
-        var date_str = date.toLocaleDateString("tr-TR", { year: 'numeric', month: 'long', day: 'numeric' });
-        str = "En son " + date_str + " tarihinde güncellendi";
-    }
+    var date_str = commit.data.commit.committer.date;
+    // Convert the date string to American date format
+    var date = new Date(date_str);
+    var us_date_str = date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
     // Set the text in the document
-    doc_element.innerText = str;
+    document.getElementById("update").innerText = "Last updated on " + us_date_str;
 }
 
 // Run the function after document is loaded
